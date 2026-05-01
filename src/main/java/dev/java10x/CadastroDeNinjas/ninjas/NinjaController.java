43415c67ea2fb37c3b4f169001f9a4ad98e0d9d9
DevLuigi <luigidasilvacoelho@gmail.com>
2026-05-01 20:58:38 -0300

    feat: Adiciona mock de endpoints ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
@@ -1,16 +1,39 @@
 package dev.java10x.CadastroDeNinjas.ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninja")
 public class NinjaController {
 
-    @GetMapping("/hello-world")
+    @GetMapping("/helloWorld")
     public String HelloWorld() {
         return "Hello world";
+    }
+
+    @GetMapping("/listarTodos")
+    public String listarTodos() {
+        return "Lista todos os ninjas";
+    }
+
+    @GetMapping("/buscarPorId")
+    public String buscarPorId() {
+        return "Lista por ID";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar() {
+        return "Salva o ninja";
+    }
+
+    @PutMapping("/alterar")
+    public String alterar() {
+        return "Altera o ninja";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletar() {
+        return "Deletar o ninja";
     }
 
 }
